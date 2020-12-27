1ab78b15ddb2b01815c7d62e5c0ecfb1479ee023
suleymankhrmn <skahraman1834@gmail.com>
2020-12-27 16:50:36 +0300

    status is changed

--- src/components/Status.svelte
+++ src/components/Status.svelte
@@ -1,20 +1,31 @@
 <script>
-    import {enablePlay} from "../store/store"
-    import {Row} from "sveltestrap";
+    import { enablePlay, intervalId, totalSeconds } from "../store/store";
+    import { Row } from "sveltestrap";
     // let enablePlay = true;
-    function buttonClick(){
-        enablePlay.update(value => !value);
-
+    function playPauseClick() {
+        enablePlay.update((value) => !value);
+        if (!$enablePlay) clearInterval($intervalId);
+        else {
+            const interval = setInterval(() => {
+                totalSeconds.update((value) => value + 1);
+            }, 1000);
+            intervalId.set(interval);
+        }
+    }
+    function stopClick() {
+        enablePlay.update((value) => !value);
+        if (!$enablePlay) clearInterval($intervalId);
+        totalSeconds.set(0);
     }
 </script>
 
 <style>
-    h3{
+    h3 {
         font-size: 3em;
         font-weight: bold;
         color: lightcoral;
     }
-    button{
+    button {
         background-color: unset;
         border: unset;
         font-size: 4em;
@@ -28,15 +39,13 @@
 
 <Row class="d-flex justify-content-center">
     {#if $enablePlay}
-    <button on:click={buttonClick}>
-        <i class="fas fa-play-circle"></i>
-    </button>
+        <button on:click={playPauseClick}>
+            <i class="fas fa-pause-circle" />
+        </button>
     {:else}
-    <button on:click={buttonClick}>
-        <i class="fas fa-pause-circle"></i>
-    </button>
+        <button on:click={playPauseClick}>
+            <i class="fas fa-play-circle" />
+        </button>
     {/if}
-    <button>
-        <i class="fas fa-stop-circle"></i>
-    </button>
+    <button on:click={stopClick}> <i class="fas fa-stop-circle" /> </button>
 </Row>
