403b79cada7ea82cfc649f99b04af5603fba2481
suleymankhrmn <skahraman1834@gmail.com>
2020-12-28 23:00:11 +0300

    status and break is added in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,8 @@
 import { writable } from 'svelte/store';
+import {Status} from "../utils/enums"
 
 export const enablePlay = writable(false);
 export const intervalId = writable();
 export const totalSeconds = writable(0);
-export const status = writable();
+export const status = writable(Status.WORKING);
+export const breakCount = writable(0);
