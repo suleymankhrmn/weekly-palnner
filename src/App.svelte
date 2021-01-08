875ee6fec9728122701c6a3afa71a8e2ea8951b8
suleymankhrmn <skahraman1834@gmail.com>
2021-01-08 08:06:10 +0300

    side areas is added

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,12 @@
 <script>
-import Status from "./components/Status.svelte";
+	import Status from "./components/Status.svelte";
 
-import Timer from "./components/Timer.svelte";
-
-
-
+	import Timer from "./components/Timer.svelte";
+	import { Col, Container, Row } from "sveltestrap";
 </script>
 
-<main>
-	<h1>Weekly Planner</h1>
-	<Status/>
-	<Timer/>
-</main>
-
 <style>
-	main {
+	.main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
@@ -25,7 +17,8 @@ import Timer from "./components/Timer.svelte";
 		color: orange;
 		text-transform: uppercase;
 		font-size: 4em;
-		font-weight:bold;
+		font-weight: bold;
+		text-align: center;
 	}
 
 	@media (min-width: 640px) {
@@ -33,4 +26,34 @@ import Timer from "./components/Timer.svelte";
 			max-width: none;
 		}
 	}
+
+	
+	@media screen and (min-width: 0px) and (max-width:900px) {
+		  .side{ display: none !important;}  /* show it on smaller screen */
+		  #phone-footer-area{
+			display: block;
+		}
+	}
+	@media screen and (min-width: 901px) {
+		  .side{ display: block !important;}  /* show it on smaller screen */
+		  #phone-footer-area{
+			display: none;
+		}
+	}
+	
 </style>
+
+<Container fluid class="h-100">
+	<Row class="justify-content-center h-100">
+		<div class="side col bg-success">sssssss</div>
+
+		<Col xl="6">
+			<h1>Weekly Planner</h1>
+			<Status />
+			<Timer />
+		</Col>
+
+		<div class="side col bg-success">ssssssss</div>
+	</Row>
+	<div id="phone-footer-area" class="row">ffffff</div>
+</Container>
